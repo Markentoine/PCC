ef3e9cb904ed4070eb9234da22532aa9ed5c7270
Jerome Toulouse <monsieur.toulouse@gmail.com>
2018-12-31 16:09:48 +0100

    Modification of the signIn form.

--- main.js
+++ main.js
@@ -1,56 +1,33 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", function (e) {
+(function () {
+    document.addEventListener("DOMContentLoaded", function (e) {
     const explanations = document.querySelector(".explanations");
-    const sentences = ['Hello World!',
-        "Vous êtes bien sur le site du PasteurCodeClub du collège de Raon.",
-        "A partir du 1er janvier, vous pourrez demander votre inscription sur ce site.",
-        "A très bientôt!",
-        "Bonnes vacances à tous!",
-    ];
 
-    const buildNewLine = sentence => {
-        document.querySelector('.explanations span').remove();
-        const div = document.createElement('div');
-        div.classList.add('line');
-        const span = document.createElement('span');
-        span.textContent = '_';
-        span.classList.add('blinking');
-        const p = document.createElement('p');
-        div.appendChild(p);
-        div.appendChild(span);
-        explanations.appendChild(div);
-        typeWriter(sentence, p);
-    };
-    const writeLines = () => {
-        let counter = 2000;
-        sentences.forEach(sentence => {
-            setTimeout(() => buildNewLine(sentence), counter);
-            counter += sentence.length * 100;
-        });
-    };
-    const countDown = () => {
-        setInterval(() => {
-            const leftTime = computeLeftTime();
-            Object.keys(leftTime).forEach(key => {
-                const parent = document.querySelector(`.${key}`);
-                const p = parent.children[1];
-                parent.children[0].remove();
-                const span = document.createElement('span');
-                span.textContent = String(leftTime[key]);
-                parent.insertBefore(span, p);
-            })
-        }, 1000);
-    };
-
-    if (dateExpired(2019, 1, 1)) {
+    if (dateExpired(2018, 11, 30)) {
         const form = document.querySelector('form');
         form.style.visibility = 'visible';
+        writeLines(sentences.after, explanations);
+    } else {
+        writeLines(sentences.before, explanations);
     }
     countDown();
-    writeLines();
 
-})
+});
+
+const countDown = () => {
+    setInterval(() => {
+        const leftTime = computeLeftTime();
+        Object.keys(leftTime).forEach(key => {
+            const parent = document.querySelector(`.${key}`);
+            const p = parent.children[1];
+            parent.children[0].remove();
+            const span = document.createElement('span');
+            span.textContent = String(leftTime[key]);
+            parent.insertBefore(span, p);
+        })
+    }, 1000);
+};
 
 const computeLeftTime = () => {
     const OPENDATE = new Date('January 7, 2019 12:35:00');
@@ -74,11 +51,28 @@ const dateExpired = (year, month, day) => {
     const currentDate = new Date();
     const toCheckDate = new Date(year, month, day);
     return currentDate >= toCheckDate;
-    //const currentDate = new Date();
-    //const currentYear = currentDate.getFullYear();
-    //const currentMonth = currentDate.getMonth() + 1;
-    //const currentDay = currentDate.getUTCDate();
-    //return currentYear >= +year && currentMonth >= +month && currentDay >= +day;
+};
+
+const writeLines = (sentences, refElement) => {
+    let counter = 2000;
+    sentences.forEach(sentence => {
+        setTimeout(() => buildNewLine(sentence, refElement), counter);
+        counter += sentence.length * 100;
+    });
+};
+
+const buildNewLine = (sentence, refElement) => {
+    document.querySelector('.explanations span').remove();
+    const div = document.createElement('div');
+    div.classList.add('line');
+    const span = document.createElement('span');
+    span.textContent = '_';
+    span.classList.add('blinking');
+    const p = document.createElement('p');
+    div.appendChild(p);
+    div.appendChild(span);
+    refElement.appendChild(div);
+    typeWriter(sentence, p);
 };
 
 const typeWriter = (string, element) => {
@@ -93,3 +87,19 @@ const typeWriter = (string, element) => {
         counter += 100;
     })
 };
+
+const sentences = {
+    before: ['Hello World!',
+        "Vous êtes bien sur le site du PasteurCodeClub du collège de Raon.",
+        "A partir du 1er janvier, vous pourrez demander votre inscription sur ce site.",
+        "A très bientôt!",
+        "Bonnes vacances à tous!",
+    ],
+    after: ['Hello World!',
+        "Bonne Année 2019!",
+        "Vous pouvez désormais demander votre inscription en remplissant le formulaire ci-dessous.",
+        "L'adresse email n'est pas obligatoire.",
+        "Happy Coding!"
+    ]
+};
+}());
